03210311d53c632b21b5475b6f67cb6c223e3673
saleh <saleh1.a.osman@gmail.com>
2019-09-15 00:16:14 +0200

    explain some idea in step2-7.js

--- javaScript/js2/week3/step2-7.js
+++ javaScript/js2/week3/step2-7.js
@@ -1,19 +1,17 @@
+//We declar here a primitive type
 const x = 9;
 function f1(val) {
   val = val + 1;
   return val;
 }
-
 f1(x);
-
 console.log(x);
-
+//We declare here an object that is a type of wraper class 
 const y = { x: 9 };
+
 function f2(val) {
   val.x = val.x + 1;
   return val;
 }
-
 f2(y);
-
 console.log(y);
