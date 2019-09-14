813da6934a102a02208be757574b0b4d6f7f3f6a
saleh <saleh1.a.osman@gmail.com>
2019-09-15 00:04:38 +0200

    create nested loop in step2-5.js

---
+++ javaScript/js2/week3/step2-5.js
@@ -0,0 +1,15 @@
+function multiplyAll(arr) {
+    // eslint-disable-next-line
+    let product = 1;
+  
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr[i].length; j++) {
+        product *= arr[i][j];
+      }
+    }
+    return product;
+  }
+  
+  const result = multiplyAll([[1, 2], [3, 4], [5, 6]]);
+  console.log('result: '+result);
+  
