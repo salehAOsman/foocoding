3f3111b602704695425c80e5596cd0aa014dc07e
saleh <saleh1.a.osman@gmail.com>
2019-09-28 20:26:52 +0200

    created assigmnet for js3 --> week3

--- javaScript/js3/week1/main.js
+++ javaScript/js3/week1/main.js
@@ -1,2 +1,48 @@
-//console.log("Hello 1");
-console.dir()
+function createNode(element) {
+    return document.createElement(element);
+}
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+
+
+const showRepoDetails = repo => event => {
+    alert(`Hello ${repo.name}`);
+    //toDo here function to display sublist for just one object and hide the other  
+    document.querySelector('#contributors').classList.add('active');
+  };
+  const createListItem = repo => {
+      
+    const ul = document.getElementById('list');
+    
+    const li = createNode('li');
+    const nameEl = createNode('span');
+    const descEl = createNode('span');
+    const Namelabel = createNode('b');
+    Namelabel.innerHTML = 'Name:';
+    const desclabel = createNode('b');
+    desclabel.innerHTML = 'Desc:';
+    nameEl.innerHTML = repo.name;
+    descEl.innerHTML = repo.description;
+    const nameText = createNode('p');
+    const descText = createNode('p');
+    append(nameText, Namelabel);
+    append(descText, desclabel);
+    append(nameText, nameEl);
+    append(descText, descEl);
+    append(li, nameText);
+    append(li, descText);
+    append(ul, li);
+    nameEl.addEventListener('click', showRepoDetails(repo));
+  };
+  const start = async url => {
+    const response = await fetch(url);
+    const repoList = await response.json();
+    repoList.forEach(createListItem);
+  };
+  const foo = start('https://api.github.com/orgs/HackYourFuture/repos?per_page=7');
+  
+  
+  
+  
+  
